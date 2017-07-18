d2284f24456c96823e73bcea87b22b353f3bfff3
jakelheknight <jakelheknight@gmail.com>
2017-07-18 09:39:50 -0600

    first hack at implementation

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # fs-globals
+## A simple FS stub for your web components
+An fs object for use in web components.
+## Usage:
+From the command line.
+```
+bower install https://github.com/fs-webdev/fs-globals.git --save
+```
+In your component.
+```javascript
+<link rel='import' src='../fs-globals'>
+```
+## Supported FS functions:
+```javascript
+  FS.simpleLocale() //Appropriate language in query param, cookie, accept language header or EN.
+  FS.locale //Array of languages based on query param, cookie, accept language header or EN.
+  FS.fetchDefaults //JSON Object of default headers and status callbacks as well as a flag for throwing errors on 400+ statuses.
+  FS.fetch(url, fetchInit, statusCallbackOverrides) //FS fetch wrapper that applies headers and status callbacks from FS.fetchDefaults.
+```
+This object will grow to encompass the full FS object.
---
+++ fs-globals.js
@@ -0,0 +1,248 @@
+(function (window, document, undefined) {
+  if (typeof window.FS === undefined) {
+    /* Global init for FS object if it doesn't exist*/
+    window.FS = {
+      JSON: {
+        parse: function (json, error) {
+          var config
+            , error = error || "Unspecified error";
+
+          try {
+            config = JSON.parse(json);
+          } catch (e) {
+            console.info("Unable to parse JSON: " + error, json);
+            console.error(e);
+            config = {};
+          }
+          return config;
+        }
+      },
+
+      htmlEncode: function (text, includeFormatting) {
+        var retVal = "";
+        if (text) {
+          retVal = text.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;").replace(/\xA0/g, "&nbsp;");
+          if (includeFormatting) {
+            retVal = retVal.replace(/\r{0,1}\n/g, "<br/>");
+            if (includeFormatting === "all") {
+              retVal = retVal.replace(/\t/g, "<span style=\"display:inline-block;width:3em;\"/>");
+            }
+          }
+        }
+        return retVal;
+      },
+
+      htmlDecode: function (html, includeFormatting) {
+        var retVal = "";
+
+        if (html) {
+          if (includeFormatting) {
+            FS._htmlDecodeDiv.innerHTML = html.replace(/\t|\r{0,1}\n/g, "").replace(/<br\s*\/{0,1}>/gi, "\n").replace(/<span style="display\:inline\-block\;width\:3em\;"\/>/g, "\t");
+          }
+          else {
+            FS._htmlDecodeDiv.innerHTML = html.replace(/\t/g, "").replace(/<br\s*\/{0,1}>/gi, "\n");
+          }
+          retVal = FS._htmlDecodeDiv.textContent;
+          FS._htmlDecodeDiv.innerHTML = ""; // Clear it out so we don't leave unused data hanging around
+        }
+
+        return retVal;
+      },
+
+      Helpers: {
+        convertStringtoBool: function (str) {
+          var bool = false;
+          if (typeof str === 'string' || str === true) {
+            bool = /^true$/i.test(str);
+          }
+          return bool;
+        },
+
+        /* Returns a function, that, as long as it continues to be invoked, will not
+         * be triggered. The function will be called after it stops being called for
+         * N milliseconds. If `immediate` is passed, trigger the function on the
+         * leading edge, instead of the trailing.
+         * Copied from Underscore.js under the following license: https://github.com/jashkenas/underscore/blob/master/LICENSE
+        */
+        debounce: function (func, wait, immediate) {
+          var timeout;
+          return function () {
+            var context = this, args = arguments;
+            var later = function () {
+              timeout = null;
+              if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+          };
+        },
+
+
+        /**
+         * Prevents a child element from scrolling a parent element (aka document).
+         * Taken from CodePen.io
+         * @see {@link http://codepen.io/Merri/pen/nhijD/}
+         * @param {Element} element - Scrolling element.
+         */
+        preventParentScroll: function (element) {
+          var html = document.getElementsByTagName('html')[0],
+            htmlTop = 0,
+            htmlBlockScroll = 0,
+            minDeltaY,
+            // this is where you put all your logic
+            wheelHandler = function (e) {
+              // do not prevent scrolling if element can't scroll
+              if (element.scrollHeight <= element.clientHeight) return;
+
+              // normalize Y delta
+              if (minDeltaY > Math.abs(e.deltaY) || !minDeltaY) {
+                minDeltaY = Math.abs(e.deltaY);
+              }
+
+              // prevent other wheel events and bubbling in general
+              if (e.stopPropagation) {
+                e.stopPropagation();
+              } else {
+                e.cancelBubble = true;
+              }
+
+              // if the height of the element is not a whole integer, we need to check for
+              // rounding errors
+              var rect = element.getBoundingClientRect()
+              var scrollHeight = element.scrollHeight + (parseInt(rect.height) - rect.height)
+
+              // most often you want to prevent default scrolling behavior (full page scroll!)
+              if ((e.deltaY < 0 && element.scrollTop === 0) ||
+                (e.deltaY > 0 && ((scrollHeight | 0) === element.scrollTop + element.clientHeight || element.scrollHeight === element.scrollTop + element.clientHeight))) {
+                if (e.preventDefault) {
+                  e.preventDefault();
+                } else {
+                  e.returnValue = false;
+                }
+              } else {
+                // safeguard against fast scroll in IE and mac
+                if (!htmlBlockScroll) {
+                  htmlTop = html.scrollTop;
+                }
+                htmlBlockScroll++;
+                // even IE11 updates scrollTop after the wheel event :/
+                setTimeout(function () {
+                  htmlBlockScroll--;
+                  if (!htmlBlockScroll && html.scrollTop !== htmlTop) {
+                    html.scrollTop = htmlTop;
+                  }
+                }, 0);
+              }
+            },
+            // here we do only compatibility stuff
+            mousewheelCompatibility = function (e) {
+              // no need to convert more than this, we normalize the value anyway
+              e.deltaY = -e.wheelDelta;
+              // and then call our main handler
+              wheelHandler(e);
+            };
+
+          // do not add twice!
+          if (element.removeWheelListener) return;
+
+          if (element.addEventListener) {
+            element.addEventListener('wheel', wheelHandler, false);
+            element.addEventListener('mousewheel', mousewheelCompatibility, false);
+            // expose a remove method
+            element.removeWheelListener = function () {
+              element.removeEventListener('wheel', wheelHandler, false);
+              element.removeEventListener('mousewheel', mousewheelCompatibility, false);
+              element.removeWheelListener = undefined;
+            };
+          }
+        }
+      }
+    };
+  }
+
+  /**
+   * Sets locale from the meta tag where snow normally sets it. If not set gives en.
+   */
+  FS.locale = FS.locale || (function () {
+    var localeMeta = document.querySelector('meta[name=allLocales]');
+    return localeMeta ? localeMeta.content.split(',') : ['en'];
+  })()
+
+  FS.simpleLocale = FS.simpleLocale || function () {
+    return FS.locale[0];
+  }
+
+  FS.dialog = FS.dialog || {
+    /**
+   * This function will register global elements and attach a reference to them to FS.dialog
+   * @param {string} elementName - The name of the element to register e.g. fs-person-card
+   * @returns {undefined} - Returns void.
+   */
+    register: function (elementName) {
+      var element = document.createElement(elementName);
+      var camelCaseName = elementName.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
+      document.body.append(element)
+      Object.defineProperty(FS.dialog, camelCaseName, {
+        get: function () {
+          return element;
+        }
+      })
+    }
+  };
+
+    /**
+     * This is a small fetch wrapper that respects the headers, status redirects and a flag from fetch defaults.
+     * @property {object} headers Key value pairs of headers to apply by default.
+     * @property {object} statusCallbacks Key value pairs of the form key=status code, value = callback
+     * @property {bool} rejectOnBadStatus Flag 
+     */
+    FS.fetchDefaults = FS.fetchDefaults || {
+      headers: {
+        "Content-Type": 'application/json',
+        "accept-language": FS.locale,
+        "Authorization": 'Bearer ' + FS.User.sessionId
+      },
+      statusCallbacks: {
+        401: function () {
+          window.location.reload();
+        }
+      },
+      rejectOnBadStatus: false
+    }
+  /**
+   * @param {string} url                  The path to the resource you are fetching
+   * @param {JSON object} fetchInit       The init object from fetch api. This is where you can do 1 time overwrites of headers as well. 
+   * @param {JSON object} statusOverrides This is where one time status callback overrides can be applied.
+   */
+  FS.fetch = function (url, fetchInit, statusOverrides) {
+  return new Promise(function (resolve, reject) {
+    if (!fetchInit) {
+      fetchInit = {
+        method: "get",
+        headers: FS.fetchDefaults.headers,
+        cach: "default"
+      };
+    }
+    var options = FS.fetchDefaults.status
+    if (statusOverrides) {
+      for (key in statusOverrides) {
+        options.status[key] = statusOverrides[key];
+      }
+    }
+    if (fetchInit.headers) {
+      fetchInit.headers = Object.assign( {}, FS.fetchDefaults.headers, fetchInit.headers);
+    }
+    fetch(url, fetchInit).then(function (res) {
+      if (options[res.status]) {
+        options[res.status]()
+      } else if (options.throw && !res.ok) {
+        reject(new Error(res));
+      } else {
+        resolve(res);
+      }
+    })
+  });
+  }
+})(window, document);
