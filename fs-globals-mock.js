36d7dd8f4e5670bde8abf83bd2881f4d386edf71
thorntonjn@familysearch.org <thorntonjn@familysearch.org>
2017-10-02 14:04:11 -0600

    Stub window.fetch and store on FS.fetchStub.
    Update bower version to 2.0.0 for breaking change.

--- fs-globals-mock.js
+++ fs-globals-mock.js
@@ -3,3 +3,8 @@ FS.User = FS.User || {};
 FS.User.sessionId = FS.User.sessionId || 'sessionId';
 FS.Analytics = { trackData:function() {}, trackLink: function() {}, trackPageView: function() {},
   trackValue: function(){}, updatePageViewData: function() {} };
+
+// WARNING: This file is intended to only be run within tests and requires sinonjs
+// Besides ensuring that window.fetch is never actually called during a test, this method
+// can be spied on or matched for specific parameters. (i.e. FS.fetchStub.withArgs(...).returns(...) )
+FS.fetchStub = FS.fetchStub || sinon.stub(window,"fetch");
