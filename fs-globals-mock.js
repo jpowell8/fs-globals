32bae08e20784c69031548f861d76d4e914e3143
Rose Marie Sadler <rose.sadler@familysearch.org>
2017-10-05 15:37:21 -0600

    adds showEx mock

--- fs-globals-mock.js
+++ fs-globals-mock.js
@@ -3,3 +3,4 @@ FS.User = FS.User || {};
 FS.User.sessionId = FS.User.sessionId || 'sessionId';
 FS.Analytics = { trackData:function() {}, trackLink: function() {}, trackPageView: function() {},
   trackValue: function(){}, updatePageViewData: function() {} };
+FS.showEx = function () { return false };
